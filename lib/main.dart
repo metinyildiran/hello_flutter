3d0c46f303909df7454f6d661755dc55d9243634
Metin Yıldıran <metin.1464@gmail.com>
2021-11-20 18:43:59 +0300

    Added button

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("App Bar Widget"),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text("Body Widget"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Text("Click"),
-        ),
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("App Bar Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.amber[600],
       ),
-    ));
+      body: Center(
+          child: TextButton.icon(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+        ),
+        onPressed: () {},
+        icon: Icon(Icons.mail),
+        label: Text("mail me"),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text("Click"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
